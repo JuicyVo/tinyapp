a94a3b642de5714bc327ca6371eb401c80d927eb
William <Williamtviet@hotmail.com>
2023-03-02 05:02:45 -0800

    Green header, fixed index

--- express_server.js
+++ express_server.js
@@ -5,8 +5,8 @@ const PORT = 8080
 app.set("view engine", "ejs")
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca"//,
-  // "9sm5xK": "http://www.google.com"
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 }
 
 app.get("/urls", (req, res) => {
