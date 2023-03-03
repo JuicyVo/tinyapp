6feb5b5b643ae2a71bef9db8e9dee4f0430ea529
William <Williamtviet@hotmail.com>
2023-03-02 22:52:23 -0800

    Added a random string geneerator

--- express_server.js
+++ express_server.js
@@ -9,9 +9,30 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+function generateRandomString() {
+  let randomString=""
+  let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"
+  for (let i=0; i<6; i++) {
+    let rnum = Math.floor(Math.random()*chars.length)
+     //chars.length so it uses all the char options
+    randomString += chars.substring (rnum, rnum+1)
+  }
+  console.log (randomString)
+}
+
+
+app.use(express.urlencoded({ extended: true}))
+
+
 app.get("/urls", (req, res) => {
   const templateVars= { urls: urlDatabase };
   res.render ("urls_index", templateVars);
+})
+
+app.get("/urls/new", (req, res) => { 
+  //has to be before app.get urls/;id to work
+  res.render ("urls_new")
+  
 })
 
 app.get("/urls/:id", (req, res) => {
@@ -21,6 +42,12 @@ app.get("/urls/:id", (req, res) => {
   // console.log (typeof urlDatabase[req.params.id]) //this line failed
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  generateRandomString()
+  res.send (randomString); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/", (req, res) => {
   res.send("Hello")
 })
@@ -28,6 +55,9 @@ app.get("/", (req, res) => {
 app.get ("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
+
+
+
 
 app.listen (PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
